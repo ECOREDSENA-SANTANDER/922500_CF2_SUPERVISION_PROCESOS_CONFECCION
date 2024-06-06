3faaed74231db7458d3d348059d0df3ad2bd4fdd
LizManchego <karinamanchego0416@gmail.com>
2024-06-06 15:12:40 -0600

    ADD CFA / Material ZIP

--- src/config/global.js
+++ src/config/global.js
@@ -168,7 +168,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CF02_922500_DU.zip',
       },
       {
         icono: 'fas fa-download',
